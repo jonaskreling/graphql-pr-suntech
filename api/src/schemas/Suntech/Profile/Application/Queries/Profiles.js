29ddddb444489b5270ed9cab6c06718d05401f6c
Jonas Supero <nilton.deolindo@supero.com.br>
2019-12-30 15:32:17 -0300

    Delete unnecessary files

--- api/src/schemas/Suntech/Profile/Application/Queries/Profiles.js
+++
@@ -1,6 +0,0 @@
-import listProfile from '../../Infrastructure/listProfile'
-
-export default async (obj, params, ctx) => {
-  ctx.authorization(ctx, 'Profile', params.filters !== undefined ? 'r' : 'b' )
-  return listProfile(ctx)
-}
